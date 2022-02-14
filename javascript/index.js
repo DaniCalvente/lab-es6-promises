66d48062c1f92a02e67b1744ec4739c9a62c495b
Dani Calvente <danicalvente@hotmail.com>
2022-02-14 19:27:56 +0100

    iteraciones 2 y 3 hechas

--- javascript/index.js
+++ javascript/index.js
@@ -76,25 +76,26 @@ obtainInstruction("steak", 0)
 // ...
 async function makeBroccoli() {
   try {
-   await obtainInstruction(0)
+    const step1 = await obtainInstruction("broccoli", 0);
     document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`;
-    const brocRecipe1 = await obtainInstruction(1)
+    const step2 = await obtainInstruction("broccoli", 1);
     document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`;
-    const brocRecipe2 = await obtainInstruction(2)
+    const step3 = await obtainInstruction("broccoli", 2);
     document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`;
-    const brocRecipe3 = await obtainInstruction(3)
+    const step4 = await obtainInstruction("broccoli", 3);
     document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`;
-    const brocRecipe4 = await obtainInstruction(4)
+    const step5 = await obtainInstruction("broccoli", 4);
     document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`;
-    const brocRecipe5 = await obtainInstruction(5)
+    const step6 = await obtainInstruction("broccoli", 5);
     document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
+    const step7 = await obtainInstruction("broccoli", 6);
+    document.querySelector("#broccoli").innerHTML += `<li>${step7}</li>`;
     document.querySelector("#broccoli").innerHTML += `<li>${`Broccoli is ready`}</li>`;
-  }
-  catch(err)  {
+  } catch (err) {
     console.log("no hay datos");
-  };
+  }
 }
-makeBroccoli()
+makeBroccoli();
 
 // Bonus 2 - Promise all
 // ...
